5433cdb4a7c27ed8a2937e488845514212447dc8
Jeno Lustyik <lustyik.jeno@gmail.com>
2022-02-25 00:01:57 +0100

    Update visualization_streamlit.py

--- visualization_streamlit.py
+++ visualization_streamlit.py
@@ -7,14 +7,13 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
-# Title
+# titles
 st.title("Annual Dystopian Book Expo")
 st.subheader("An insight into the best Dystopian books and authors")
 
-headers = ['url', 'title', 'author', 'num_ratings', 'num_reviews', 'avg_rating', 'pages', 'publish_date', 'series',
+headers = ['url', 'titles', 'author', 'num_ratings', 'num_reviews', 'avg_rating', 'num_pages', 'publish_date', 'series',
            'genres', 'awards', 'places']
-df = pd.read_csv(r'D:\Users\lusty\Strive\GitHub\build-week-gryffindor\final.csv', header=None)
-df.columns = headers
+df = pd.read_csv(r'D:\Users\lusty\Strive\GitHub\build-week-gryffindor\preprocessed.csv', header=0)
 
 if st.button('Show data'):
     st.dataframe(df)
@@ -23,12 +22,30 @@ book_input = st.text_input('Author\'s name')
 df1 = df.loc[df['author'].str.contains(book_input, flags=re.IGNORECASE)]
 if len(book_input) > 0:
     st.dataframe(df1)
+# num_pages / Ratings relationship
 
-# Pages / Ratings relationship
+genre_selector = st.selectbox(label='Select a genre', options=df['genres'])
+df2 = df.loc[df['num_pages'] != 0]
+df2 = df2.loc[df['num_pages'] < df['num_pages'].quantile(q=0.95)]
+df2 = df2.loc[df['num_ratings'] < df['num_ratings'].quantile(q=0.95)]
 
-df2 = df.loc[df['pages'] != 0]
-fig_scatter = px.scatter(df2, x=df2['pages'], y=df2['avg_rating'], color=df2['avg_rating'])
-st.plotly_chart(fig_scatter)
+df2 = df2.loc[df['num_pages'] > df['num_pages'].quantile(q=0.05)]
+df2 = df2.loc[df['num_ratings'] > df['num_ratings'].quantile(q=0.05)]
+
+fig_pages = make_subplots(cols=2, rows=1)
+fig_pages.add_trace(go.Scatter(x=df2['num_pages'], y=df2['minmax_norm_ratings'], mode='markers', name='Pages Scatter'), col=1, row=1)
+fig_pages.add_trace(go.Histogram(x=df2['num_pages'], y=df2['minmax_norm_ratings'], histfunc='avg', nbinsx=10, name='Pages Histogram'), col=2, row=1)
+fig_pages.update_layout(bargap=0.1)
+
+st.plotly_chart(fig_pages)
+
+#
+# fig_scatter = px.scatter(df2, x=df2['num_pages'], y=df2['minmax_norm_ratings'], color=df2['minmax_norm_ratings'])
+# st.plotly_chart(fig_scatter)
+# fig_hist_ratings = px.histogram(df2, x=df2['num_pages'], y=df2['minmax_norm_ratings'], nbins=10, histfunc='avg')
+# fig_hist_ratings.update_layout(bargap=0.1)
+# st.plotly_chart(fig_hist_ratings)
+
 
 # Locations in the book: is a book rated higher if it happens in america?
 
@@ -45,10 +62,10 @@ df6 = df6.sort_values(by='avg_rating')
 
 fig_places = make_subplots(rows=1, cols=2)
 fig_places.add_scatter(
-    x=df5['title'], y=df5['avg_rating'],
+    x=df5['titles'], y=df5['avg_rating'],
     row=1, col=1)
 fig_places.add_scatter(
-    x=df6['title'], y=df6['avg_rating'],
+    x=df6['titles'], y=df6['avg_rating'],
     row=1, col=2)
 st.plotly_chart(fig_places)
 st.dataframe(df6)
